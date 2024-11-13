9a7dd882207c625ff80460de9dc2f4a5b2f93847
Dawan Sawyer <Dawansawyer@gmail.com>
2024-11-13 13:08:29 -0500

    Feat(components): Add size and weight variants to styled text component Implement reusable text component with small, big, and bold variants Add dynamic font sizing and weight controls Include color theming from config

--- components/Texts/StyledText.js
+++ components/Texts/StyledText.js
@@ -1,5 +1,19 @@
-import { Text } from 'react-native';
-
-export const StyledText = ({ children }) => {
-  return <Text>{children}</Text>;
+import { Text } from "react-native";
+import { colors } from "../../config/theme";
+export const StyledText = ({ children, small, big, bold, style, ...props }) => {
+  return (
+    <Text
+      style={[
+        {
+          color: colors.tint,
+          fontSize: small ? 12 : big ? 20 : 16,
+          fontWeight: bold || big ? "bold" : "normal",
+        },
+        style,
+      ]}
+      {...props}
+    >
+      {children}
+    </Text>
+  );
 };
