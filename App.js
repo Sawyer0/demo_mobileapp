f078af25ad57ecde7141c631a562db51c55025f8
Dawan Sawyer <Dawansawyer@gmail.com>
2024-11-13 20:59:17 -0500

    feat: Add splash screen and app initialization logic Implement SplashScreen to handle app loading state Add async prepareApp function for initialization tasks Configure splash screen prevention during load Set up error handling for initialization process Prepare structure for future backend data fetching Integrate with NavigationContainer for post-load navigation
    This commit adds proper app initialization handling with
    a splash screen, ensuring a smooth user experience while
    the app prepares necessary resources and data.

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
+import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { Home } from "./screens";
+import * as SplashScreen from "expo-splash-screen";
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const prepareApp = async () => {
+    try {
+      //fetch data from "backend"
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      await SplashScreen.hideAsync();
+    }
+  };
+  useEffect(() => {
+    prepareApp();
+  }, []);
   return (
     <NavigationContainer>
-      <Home/>
+      <Home />
       <StatusBar style="auto" />
     </NavigationContainer>
   );
